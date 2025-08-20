f275ceb2aa41a77096b9ff2ce24865ff3a779f82
petDev <olqi@me.com>
2025-08-20 15:29:10 +0700

    setup nativewind

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+import "./globals.css";
+
+export default function Index() {
+  return (
+    <View
+    >
+      <Text
+      className="text-white"
+      >Edit</Text>
+    </View>
+  );
+}
