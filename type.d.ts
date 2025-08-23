bc79d13d77fa4673e268394671f9cd030f92ebaf
petDev <olqi@me.com>
2025-08-23 16:31:14 +0700

    add profile swiping

--- type.d.ts
+++ type.d.ts
@@ -2,3 +2,9 @@ export interface TabBarIconProps {
     focused: boolean;
     icon: ImageSourcePropType
 }
+
+export interface ProfileCardProps {
+  profileImage: ImageSourcePropType;
+  profileName: string;
+  profileDescription: string;
+}
