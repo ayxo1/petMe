c6cc1b1b924a39db8c6fb5f02144d0e44a06fa53
petDev <olqi@me.com>
2025-08-25 12:53:39 +0700

    add a shelter page

---
+++ app/(tabs)/shelter.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const shelter = () => {
+  return (
+    <View>
+      <Text>shelter</Text>
+    </View>
+  )
+}
+
+export default shelter
